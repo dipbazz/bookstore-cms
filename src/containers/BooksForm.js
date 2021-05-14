e6c4bb81b56e5815dcf26f48ac9ecb711605a660
Dipesh Bajgain <dipbazz@gmail.com>
2021-05-14 16:24:53 +0545

    Update booksform component
    BooksForm component display a form with input to take title and
    select input to display the list of book category

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,7 +1,21 @@
 const BooksForm = () => {
+  const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
   return (
     <>
-      Books form.
+      <form action="#">
+        <div>
+          <label>Book title</label>
+          <input type="text" placeholder="Book title" />
+        </div>
+
+        <div>
+          <label>Book category</label>
+          <select>
+            {categories.map(category => <option value={category}>{category}</option>)}
+          </select>
+        </div>
+
+      </form>
     </>
   )
 }
