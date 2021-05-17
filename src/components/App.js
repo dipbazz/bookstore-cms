6c71186a910b7e7b895e9c81e4cdca85653d3742
Dipesh Bajgain <dipbazz@gmail.com>
2021-05-17 15:02:36 +0545

    Update book render with filter
    All the books before rendering in the dom it will
    be filtered to check the filter status of the category
    form redux store and displays only the filtered books

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import '../App.css';
 import BookList from '../containers/BookList';
 import BooksForm from '../containers/BooksForm';
-import CategoryFilter from './CategoryFilter';
 
 function App() {
   return (
     <div className="App">
-      <CategoryFilter />
       <BookList />
       <BooksForm />
     </div>
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,14 +1,20 @@
-const CategoryFilter = () => {
+import PropTypes from 'prop-types';
+
+const CategoryFilter = ({ handleFilterChange }) => {
   const categories = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
   return (
     <label htmlFor="category">
       Book category
-      <select id="category" name="category" required>
+      <select id="category" name="category" onChange={handleFilterChange} required>
         {categories.map((item) => <option key={item} value={item}>{item}</option>)}
       </select>
     </label>
   );
+};
+
+CategoryFilter.propTypes = {
+  handleFilterChange: PropTypes.func.isRequired,
 };
 
 export default CategoryFilter;
